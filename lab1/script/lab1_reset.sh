f4e811bb58b00164714adc2518da7ea96ccf6c93
sunwoongkim <79438891+sunwoongkim@users.noreply.github.com>
2021-02-21 22:54:05 -0800

    Update lab1_reset.sh

--- lab1/script/lab1_reset.sh
+++ lab1/script/lab1_reset.sh
@@ -1,2 +1,2 @@
-echo 0 > /sys/class/gpio/gpio17/value						# set gpio17/value to 0
-echo 17 > /sys/class/gpio/unexport							# delete the gpio17 directory
+echo 0 > /sys/class/gpio/gpio17/value	            # set gpio17/value to 0
+echo 17 > /sys/class/gpio/unexport                # delete the gpio17 directory
